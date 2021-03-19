fe454daf0b33be09312c6e7bf8995bbdf067b9ab
jcms-xyz <juan.carlo.santos@eee.upd.edu.ph>
2021-03-20 00:26:40 +0800

    added classes

---
+++ src/Dog.java
@@ -0,0 +1,24 @@
+public class Dog extends Animal{
+
+    public void digHole(){
+        System.out.println("Dug a hole");
+    }
+
+    public Dog(){
+        super();
+        setSound("Bark");
+    }
+
+    public void changeVar(int randNum){
+        randNum = 12;
+        System.out.println("randNum in method: " + randNum);
+    }
+
+    private void bePrivate(){
+        System.out.println("private!");
+    }
+
+    public void accessPrivate(){
+        bePrivate();
+    }
+}

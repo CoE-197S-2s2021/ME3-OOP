41ced9996e3372ad99c117771d4a8199359f30d0
Nola <paolo.nolasco@eee.upd.edu.ph>
2021-03-23 03:02:18 +0800

    finished the activity

---
+++ src/Monkey.java
@@ -0,0 +1,61 @@
+public class Monkey implements Living {
+    @Override
+    public void setName(String newName) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setHeight(String newHeight) {
+
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void setWeight(String newWeight) {
+
+    }
+
+    @Override
+    public double getWeight() {
+        return 0;
+    }
+
+    @Override
+    public void setFavFood(String newFavFood) {
+
+    }
+
+    @Override
+    public String getFavFood() {
+        return null;
+    }
+
+    @Override
+    public void setSpeed(String newSpeed) {
+
+    }
+
+    @Override
+    public double getSpeed() {
+        return 0;
+    }
+
+    @Override
+    public void setSound(String newSound) {
+
+    }
+
+    @Override
+    public String getSound() {
+        return null;
+    }
+}

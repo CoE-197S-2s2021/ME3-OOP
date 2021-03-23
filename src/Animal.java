8f9187d0df9cf15aa7118558b1bc0d6160b8fd13
astilagmay <lagmayarnold@gmail.com>
2021-03-23 17:37:30 +0800

    Initial Commit (Finished)

---
+++ src/Animal.java
@@ -0,0 +1,53 @@
+public class Animal {
+
+    // Attributes
+    private String name;
+    private double height;
+    private int weight;
+    private String favFood;
+    private double speed;
+    private String sound;
+
+    // Public Methods
+    public void setName(String newName) { name = newName; }
+    public String getName() { return name; }
+
+    public void setHeight(double newHeight) { height = newHeight; }
+    public double getHeight() { return height; }
+
+    public void setWeight (int newWeight) {
+        if (newWeight > 0) {
+            weight = newWeight;
+        }
+
+        else {
+            System.out.println("Weight must be bigger than 0");
+        }
+    }
+    public double getWeight() { return weight; }
+
+    public void setFavFood(String newFavFood) { favFood = newFavFood; }
+
+    public String getFavFood() { return favFood; }
+
+    public void setSpeed(double newSpeed) { speed = newSpeed; }
+
+    public double getSpeed() { return speed; }
+
+    public void setSound(String newSound) { sound = newSound; }
+
+    public String getSound() { return sound; }
+
+    // Private Methods
+    private void bePrivate() {
+        System.out.println("I'm a private method");
+    }
+
+    // Main
+    public static void main(String[] args) {
+        Animal dog = new Animal();
+        dog.setName("Grover");
+        System.out.println(dog.getName());
+    }
+
+}

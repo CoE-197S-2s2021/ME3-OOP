160b09b857f747a3d9d76a0e5fc31fb519da3033
TotalUpbeat <66831800+TotalUpbeat@users.noreply.github.com>
2021-03-24 02:35:12 +0800

    Added all relevant Files :)

---
+++ src/com/company/Cat.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Cat extends Animal{
+    public void digHole(){
+        System.out.println("Dug a hole");
+    }
+    public Cat(){
+        super();
+        setSound("Meow");
+    }
+}
---
+++ src/com/company/Giraffe.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Giraffe extends Creature{
+    private String name;
+
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void setWeight(double newWeight){
+        // return null;
+    }
+    public double getWeight(){
+        return 0;
+    }
+
+}

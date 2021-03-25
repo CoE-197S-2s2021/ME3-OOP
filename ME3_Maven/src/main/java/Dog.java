ce8174e21eda48f8bd238c012466481ca1e5a5ca
seancabalse.dev <seancabalse.dev@gmail.com>
2021-03-26 02:04:34 +0800

    Added Dog.java class

---
+++ ME3_Maven/src/main/java/Dog.java
@@ -0,0 +1,12 @@
+public class Dog extends Animal {
+
+    //Method Declaration
+    public void digHole(){
+        System.out.println("Dug a hole");
+    }
+    public Dog(){
+        super();
+
+        setSound("Bark");
+    }
+}

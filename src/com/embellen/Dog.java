7fb579f6413099ef06e75a75175ec9cb4b378768
Enrique <enriqueiiibellen@gmail.com>
2021-04-03 20:04:38 +0800

    added files for Basic OOP Concepts Part 1

---
+++ src/com/embellen/Dog.java
@@ -0,0 +1,17 @@
+package com.embellen;
+
+public class Dog extends Animal {
+    public void digHole() {
+        System.out.println("Dug a hole");
+    }
+
+    public Dog() {
+        this.setSound("Bark");
+    }
+
+    public void changeVar(int randNum) {
+
+        randNum = 12;
+        System.out.println("randNum in method: " + randNum);
+    }
+}

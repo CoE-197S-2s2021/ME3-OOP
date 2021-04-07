be200aed6817edf4180fbcc80d1c3e0c5e644946
Harv <kbdelacruz7@up.edu.ph>
2021-04-07 20:31:57 +0800

    update dog class

--- src/com/DC/Dog.java
+++ src/com/DC/Dog.java
@@ -15,4 +15,11 @@ public class Dog extends Animal{
         randNum = 12;
         System.out.println("randNum in method: " + randNum);
     }
+    private void bePrivate(){
+        System.out.println("In a private method");
+    }
+
+    public void accessPrivate(){
+        bePrivate();
+    }
 }

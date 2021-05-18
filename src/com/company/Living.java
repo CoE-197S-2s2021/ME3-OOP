15bf44cbb1a326381076983a7de57be838a92778
Larry <laurentino.garriz@eee.upd.edu.ph>
2021-05-18 23:45:04 +0800

    Completed OOP Tutorials

---
+++ src/com/company/Living.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public interface Living {
+
+    public void setName(String newName);
+    public String getName();
+
+    public void setHeight(String newHeight);
+    public double getHeight();
+
+    public void setWeight(double newWeight);
+    public double getWeight();
+
+    public void setFavFood(String newFavFood);
+    public String getFavFood();
+
+    public void setSpeed(double newSpeed);
+    public double getSpeed();
+
+    public void setSound(String newSound);
+    public String getSound();
+
+}

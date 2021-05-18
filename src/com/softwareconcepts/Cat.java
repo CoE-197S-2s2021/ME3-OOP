5e5b6416b3c3fe94ef44a166a386dfc837d39650
Micah Angelo Bacani <micah.angelo.bacani@eee.upd.edu.ph>
2021-05-18 22:13:09 +0800

    Complete Submission

---
+++ src/com/softwareconcepts/Cat.java
@@ -0,0 +1,17 @@
+package com.softwareconcepts;
+public class Cat extends Animal{
+
+    public Cat(){
+
+        super();
+
+        setSound("Meow");
+
+    }
+
+    final void attack(){
+    }
+
+    public static final double FAVNUMBER = 3.14;
+
+}

6d71b06b3f4ef6e3e4731216aea9a951ca633d0b
Micah Angelo Bacani <micah.angelo.bacani@eee.upd.edu.ph>
2021-05-18 20:40:12 +0800

    Resubmission

---
+++ Dog.java
@@ -0,0 +1,33 @@
+public class Dog extends Animal{
+
+    public void digHole(){
+
+        System.out.println("Dug a hole");
+
+    }
+
+    public void changeVar(int randNum){
+
+        randNum = 12;
+
+        System.out.println("randNum in method value: " + randNum);
+
+    }
+
+    private void bePrivate(){
+        System.out.println("In a private method");
+    }
+
+    public void accessPrivate(){
+        bePrivate();
+    }
+
+    public Dog(){
+
+        super();
+
+        setSound("Bark");
+
+    }
+
+}

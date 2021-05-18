a4c6ea4ef5b68eaa08a06951e0d895a0641cc137
AndrewGarcia568 <45362746+AndrewGarcia568@users.noreply.github.com>
2021-05-18 20:53:37 +0800

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,16 @@
+public class Cat extends Animal{
+
+    public Cat(){
+
+        super();
+
+        setSound("Meow");
+
+    }
+
+    final void attack(){
+    }
+
+    public static final double FAVNUMBER = 3.14;
+
+}

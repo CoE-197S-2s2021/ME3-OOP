6e43831e175b5c1add1d26518a9cfc6409760e64
avarixa <45745951+avarixa@users.noreply.github.com>
2021-06-24 02:25:52 +0800

    reupload of my submissions

--- src/WorkWithAnimals.java
+++ src/WorkWithAnimals.java
@@ -1,8 +1,8 @@
-public class WorkWithAnimals{
+public class WorkWithAnimals {
 
-    //int justANum = 10;
+    int justANum = 10;
 
-    public static void main(String[] args) {
+    public static void main (String[] args){
 
         Dog fido = new Dog();
 
@@ -10,10 +10,10 @@ public class WorkWithAnimals{
         System.out.println(fido.getName());
 
         fido.digHole();
-
         fido.setWeight(-1);
 
         int randNum = 10;
+
         fido.changeVar(randNum);
 
         System.out.println("randNum after method call: " + randNum);
@@ -26,30 +26,22 @@ public class WorkWithAnimals{
         Animal kitty = new Cat();
 
         System.out.println("Doggy says: " + doggy.getSound());
-        System.out.println("Kitty says: " + kitty.getSound() + "\n");
+        System.out.println("Kitty says: " + kitty.getSound());
 
         Animal[] animals = new Animal[4];
         animals[0] = doggy;
         animals[1] = kitty;
 
         System.out.println("Doggy says: " + animals[0].getSound());
-        System.out.println("Kitty says: " + animals[1].getSound() + "\n");
+        System.out.println("Kitty says: " + animals[1].getSound());
 
         speakAnimal(doggy);
 
-        ((Dog) doggy).digHole();
+        ((Dog)doggy).digHole();
 
-        //System.out.println(justANum);
-        //sayHello();
-
-        // Can't call a private method even when defined in
-        // the subclass
-
-        //fido.bePrivate();
-
-        // Execute a private method by using another public
-        // method in the class
-
+//        System.out.println(justANum);
+//        sayHello();
+//        fido.bePrivate();
         fido.accessPrivate();
 
         Giraffe giraffe = new Giraffe();
@@ -57,28 +49,19 @@ public class WorkWithAnimals{
         giraffe.setName("Frank");
 
         System.out.println(giraffe.getName());
-
     }
 
     public static void changeObjectName(Dog fido){
-
         fido.setName("Marcus");
-
     }
 
     public static void speakAnimal(Animal randAnimal){
-
         System.out.println("Animal says: " + randAnimal.getSound());
 
     }
 
-    // Non-static method to demonstrate that you can't
-    // call a non-static method inside a static method
-
-    //public void sayHello(){
-
-        //System.out.println("Hello");
-
-    //}
+    public void sayHello(){
+        System.out.println("Hello");
+    }
 
 }

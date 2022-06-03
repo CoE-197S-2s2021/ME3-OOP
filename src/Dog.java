b3b7488c30ccb0549897c23f8f7fdb4aa0ad49fe
Richard <richard.mendoza.emp@gmail.com>
2022-06-03 22:20:26 +0800

    ME3-oop excluding bonus

--- src/Dog.java
+++ src/Dog.java
@@ -1,6 +1,8 @@
 public class Dog extends Animal{
 
-    public void digHole(){ System.out.println("Dug a hole"); }
+    public void digHole(){
+        System.out.println("Dug a hole");
+    }
 
     public void changeVar(int randNum){
         randNum = 12;
---
+++ src/com/codewithrichard/Main.java
@@ -0,0 +1,8 @@
+package com.codewithrichard;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
